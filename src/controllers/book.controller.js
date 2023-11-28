ce6e37c4dce94010783da23d0a27c81dedd8eac0
Jona <ezeqjona98@gmail.com>
2023-11-28 20:48:40 -0300

    Implementación endpoint DELETE byId de Libros

--- src/controllers/book.controller.js
+++ src/controllers/book.controller.js
@@ -1,8 +1,24 @@
 import { BookService } from "../services/book.service.js";
 
-const getAll = (req, res, next) => {
-    BookService.getAll()
-        .then((result) => {
-            res.status(200).json({data: result})
-        })
+const getById = async (req, res, next) => {
+    try {
+        const result = await BookService.getById(req.params.id);
+        res.status(200).json({ data: result });
+    } catch (error) {
+        next(error);
+    }
+};
+
+const remove = async (req, res, next) => {
+    try {
+        const result = await BookService.remove(req.params.id);
+        res.status(200).json({ data: result });
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const BookController = {
+    getById,
+    remove
 }
