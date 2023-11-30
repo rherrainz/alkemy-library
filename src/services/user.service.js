4f232ddaefa7c677a130e4f1244d42e54ea5a61e
Dylan Erdini <erdini.dylan@gmail.com>
2023-11-29 23:15:02 -0300

    Implementando los services de user

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,9 +1,29 @@
 import { UserRepository } from "../repositories/user.repository.js"
 
+const getAll = async () => {
+    return await BookRepository.getAll();
+}
+
 const getById = async (id) => {
     return await UserRepository.getById(id)
 }
 
+const create = async (user) => {
+    return await UserRepository.create(user)
+}
+
+const update = async (id, user) => {
+    return await UserRepository.update(id, book)
+}
+
+const remove = async (id) => {
+    return await UserRepository.remove(id)
+}
+
 export const UserService = {
-    getById
+    getAll,
+    getById,
+    create,
+    update,
+    remove
 }
